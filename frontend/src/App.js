d11d88b3e7de2e666a43da3f050fae1f6acdbbfa
LOkn7630 <loknath@iitg.ac.in>
2021-04-13 00:39:43 +0530

    sp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import HomeScreen from "./screens/users/HomeScreen";
-import AdminScreen from "./screens/admin/AdminScreen";
-import "./assets/main.css";
-import { fetchUserAction } from "./actions/auth";
-import NotFound from "./components/NotFound";
-import Footer from "./components/Home/Footer/Footer";
-import EventCard from "./components/Home/Footer/EventCard";
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import HomeScreen from './screens/users/HomeScreen';
+import AdminScreen from './screens/admin/AdminScreen';
+import './assets/main.css';
+import { fetchUserAction } from './actions/auth';
+import NotFound from './components/NotFound';
+import Footer from './components/Home/Footer/Footer';
+import EventCard from './components/Home/Footer/EventCard';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,9 +20,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/sa/admin" component={AdminScreen} />
-        <Route path="/sa/" component={HomeScreen} />
-        <Route path="*" exact={true} component={NotFound} />
+        <Route path='/sa/admin' component={AdminScreen} />
+        <Route path='/sa/' component={HomeScreen} />
+        <Route path='*' exact={true} component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
