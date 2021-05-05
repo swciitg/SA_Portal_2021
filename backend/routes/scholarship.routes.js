35bcbb06964f88783bb5151cbeca01f86ca33bae
Sarthak <sarthak.bhagwat@rediffmail.com>
2021-05-05 19:42:19 +0530

    import fix

--- backend/routes/scholarship.routes.js
+++ backend/routes/scholarship.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { isLoggedIn } = require("../middlewares");
+const { isLoggedIn } = require("../middlewares/auth");
 
 router.get("/", (req, res) => {
   console.log("csdf");
