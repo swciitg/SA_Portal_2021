4264b0163d4d417a2b838158d023fd431f616a48
s-bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-05-07 00:06:12 +0530

    event updated

--- backend/routes/home/events.routes.js
+++ backend/routes/home/events.routes.js
@@ -5,14 +5,21 @@ const eventsController = require("../../controllers/home/eventsController");
 const multerMiddleware = require("../../middlewares/multer");
 const { isLoggedIn } = require("../../middlewares/auth");
 
+/*### Routes which doesn't need authentication ###*/
 router.get("/", eventsController.getAllEvents);
+router.post("/find", eventsController.findEvent);
+
+/*### Routes which need authentication ###*/
 router.post(
   "/",
-  isLoggedIn,
   multerMiddleware.thumbnailImageUploadMiddleware,
   eventsController.createEvent
 );
-router.delete("/:id", isLoggedIn, eventsController.deleteEvent);
-router.put("/:id", isLoggedIn, eventsController.updateEvent);
+router.delete("/:id", eventsController.deleteEvent);
+router.put(
+  "/:id",
+  multerMiddleware.thumbnailImageUploadMiddleware,
+  eventsController.updateEvent
+);
 
 module.exports = router;
