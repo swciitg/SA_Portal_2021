0887c49b8682ff4b2bab57bc80d3688c20a8f94f
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2021-05-06 12:07:13 +0530

    announcements completed

---
+++ frontend/src/components/Home/Announcement/AnnouceCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { isoToDate } from "./util";
+import arrow from "../../../assets/ArrowRight.svg";
+
+const AnnouceCard = ({ creation, title, description, link, category }) => {
+  return (
+    <div
+      className="w-full px-5 py-3 bg-white rounded"
+      style={{ boxShadow: "0px 2px 12px rgba(30, 37, 50, 0.14)" }}
+    >
+      <p className="text-xs mb-0.5" style={{ color: "#111" }}>
+        {isoToDate(creation)}
+      </p>
+      <div className="flex w-full">
+        <p className="inline text-base w-11/12">{title}</p>
+        <span className="w-1/12 flex justify-end cursor-pointer">
+          <a href={link} target="_blank">
+            <img src={arrow} alt="->" />
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default AnnouceCard;
--- frontend/src/components/Home/Announcement/Announcement.jsx
+++ frontend/src/components/Home/Announcement/Announcement.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listAnnouncement } from "../../../actions/announcement";
+import { renderCards } from "./util";
 
 function Announcement() {
+  const announcements = useSelector((state) => state.announcements);
+  const [aCategory, setACategory] = useState("all");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(listAnnouncement());
+  }, [dispatch]);
+
   return (
     <div className="w-full sm:w-7/12 md:w-3/4 p-5 sm:pl-32">
-      <p className="text-4xl font-semibold">Announcements</p>
+      <div className="mb-4 sm:mb-8">
+        <p className="text-3xl sm:text-4xl font-semibold">Announcements</p>
+      </div>
+      <div className="annoucement_card_container w-full sm:w-11/12">
+        {renderCards(announcements, aCategory)}
+      </div>
     </div>
   );
 }
