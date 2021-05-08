dedaaae8f7d499d7d24cf12db7127f2c6953f927
LOkn7630 <loknath@iitg.ac.in>
2021-05-08 11:07:57 +0530

    some changes

--- frontend/src/screens/users/Forms/FormScreen.js
+++ frontend/src/screens/users/Forms/FormScreen.js
@@ -5,14 +5,15 @@ import "./FormScreen.css"
 const FormScreen = () => {
   return (
         <div className='container mx-auto px-4 md:px-48 py-5'>
+            <div class="md:ml-10" style={{'font-weight': '600' ,'font-size': '34px'}}>Forms</div>
             <table class="table-fixed w-full md:mx-5 my-4 text-center" style={{'box-shadow': 'rgba(0, 0, 0, 0.25) 0px 5px 15px'}}>
                 <tbody class=" h-5 bg-gray-500">
                     <tr>
-                    <td class="w-1/8 p-1  text-white dd">Title</td>
-                    <td class="w-1/2 text-white dd">Author</td>
-                    <td class="w-1/8 text-white dd">Views</td>
-                    <td class="w-1/8 text-white dd">Views</td>
-                    <td class="w-1/8 text-white">Views</td>
+                    <td class="w-1/8 p-1  text-white dd">Form No.</td>
+                    <td class="w-1/2 text-white dd">Subject</td>
+                    <td class="w-1/8 text-white dd">Format</td>
+                    <td class="w-1/8 text-white dd">Date</td>
+                    <td class="w-1/8 text-white">Time</td>
                     </tr>
                 </tbody>
             </table>
