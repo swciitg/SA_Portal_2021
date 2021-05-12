896d9d83c3a79a146744618fe283fd940728a497
choudhary463 <anmolchoudhary463@email.com>
2021-05-12 11:36:27 +0530

    added team and fixed scholarship

--- backend/models/scholarship/scholarship.pdf.js
+++ backend/models/scholarship/scholarship.pdf.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const ScholarshipPdfSchema = new mongoose.Schema({
+  name: {type : String, required: true},
   path: { type: String, required: true },
   creation: { type: Date, default: Date.now },
 });
