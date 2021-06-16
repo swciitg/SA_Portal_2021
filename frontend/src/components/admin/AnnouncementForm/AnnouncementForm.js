63b74496baff8916b1663b4812d88437368cb210
Pranjal Chourasia <61688724+pranjal5399@users.noreply.github.com>
2021-06-16 23:31:51 +0530

    minor changes

--- frontend/src/components/admin/AnnouncementForm/AnnouncementForm.js
+++ frontend/src/components/admin/AnnouncementForm/AnnouncementForm.js
@@ -60,108 +60,120 @@ const AnnouncementForm = ({ type, formData }) => {
 
   return (
     <>
-      <p className="text-xl pb-6 flex items-center">
-        <i className="fas fa-list mr-3"></i> {type} Announcement
-      </p>
-      <div className="leading-loose">
-        <form
-          className="p-10 bg-white rounded shadow-xl"
-          onSubmit={(e) => formSubmitHandler(e)}
-        >
-          <div className="">
-            <label className="block text-sm text-gray-600" htmlFor="title">
-              Title
-            </label>
-            <input
-              className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
-              id="title"
-              name="title"
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Title"
-              required
-            />
-          </div>
-          <div className="mt-2">
-            <label className="block text-sm text-gray-600" htmlFor="link">
-              Link
-            </label>
-            <input
-              className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
-              id="link"
-              name="link"
-              type="text"
-              value={link}
-              required
-              onChange={(e) => setLink(e.target.value)}
-              placeholder="Your Link"
-            />
-          </div>
-          <div className="mt-2">
-            <label
-              className=" block text-sm text-gray-600"
-              htmlFor="description"
-            >
-              Description
-            </label>
-            <textarea
-              className="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded"
-              id="description"
-              name="description"
-              rows="4"
-              required
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Your description"
-            ></textarea>
-          </div>
+      <h1 class="text-3xl text-black pb-6">{type} Forms</h1>
 
-          <div className="mt-2">
-            <label className="block text-sm text-gray-600" htmlFor="category">
-              Category
-            </label>
-            <input
-              className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded"
-              list="categories"
-              id="category"
-              type="text"
-              name="category"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              required
-            />
-            <datalist id="categories">
-              {categories.map((category, key) => {
-                return <option key={key} value={category.name} />;
-              })}
-            </datalist>
-          </div>
-
-          <div className="mt-2">
-            <label className="block text-sm text-gray-600" htmlFor="important">
-              <input
-                className="mr-2"
-                id="important"
-                type="checkbox"
-                name="impprtant"
-                onChange={() => {
-                  setImportant(!important);
-                }}
-                checked={important}
-              />
-              Important
-            </label>
-          </div>
-          <div className="mt-6">
-            <button
-              className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
-              type="submit"
+      <div class="flex flex-wrap justify-center">
+        <div class="w-full lg:w-1/2 my-6 pr-0 lg:pr-2">
+          <p className="text-xl pb-6 flex items-center">
+            <i className="fas fa-list mr-3"></i> {type} Announcement
+          </p>
+          <div className="leading-loose">
+            <form
+              className="p-10 bg-white rounded shadow-xl"
+              onSubmit={(e) => formSubmitHandler(e)}
             >
-              Submit
-            </button>
+              <div className="">
+                <label className="block text-sm text-gray-600" htmlFor="title">
+                  Title
+                </label>
+                <input
+                  className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                  id="title"
+                  name="title"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="Title"
+                  required
+                />
+              </div>
+              <div className="mt-2">
+                <label className="block text-sm text-gray-600" htmlFor="link">
+                  Link
+                </label>
+                <input
+                  className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+                  id="link"
+                  name="link"
+                  type="text"
+                  value={link}
+                  required
+                  onChange={(e) => setLink(e.target.value)}
+                  placeholder="Your Link"
+                />
+              </div>
+              <div className="mt-2">
+                <label
+                  className=" block text-sm text-gray-600"
+                  htmlFor="description"
+                >
+                  Description
+                </label>
+                <textarea
+                  className="w-full px-5 py-2 text-gray-700 bg-gray-200 rounded"
+                  id="description"
+                  name="description"
+                  rows="4"
+                  required
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  placeholder="Your description"
+                ></textarea>
+              </div>
+
+              <div className="mt-2">
+                <label
+                  className="block text-sm text-gray-600"
+                  htmlFor="category"
+                >
+                  Category
+                </label>
+                <input
+                  className="w-full px-5  py-4 text-gray-700 bg-gray-200 rounded"
+                  list="categories"
+                  id="category"
+                  type="text"
+                  name="category"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  required
+                />
+                <datalist id="categories">
+                  {categories.map((category, key) => {
+                    return <option key={key} value={category.name} />;
+                  })}
+                </datalist>
+              </div>
+
+              <div className="mt-2">
+                <label
+                  className="block text-sm text-gray-600"
+                  htmlFor="important"
+                >
+                  <input
+                    className="mr-2"
+                    id="important"
+                    type="checkbox"
+                    name="impprtant"
+                    onChange={() => {
+                      setImportant(!important);
+                    }}
+                    checked={important}
+                  />
+                  Important
+                </label>
+              </div>
+              <div className="mt-6">
+                <button
+                  className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
