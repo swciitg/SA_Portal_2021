0f437f93c8aa1b5b8ca67ed43e0caa4507c08abb
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2021-07-21 21:49:57 +0530

    SAC ui changed to that of Utils

--- frontend/src/screens/users/sac/SACScreen.js
+++ frontend/src/screens/users/sac/SACScreen.js
@@ -1,70 +1,33 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import "./SACScreen.css";
-// import { listSACs } from "../../../actions/sac";
-// import { BASEAPI } from "../../../constants";
-// import ColorMarker from "../../../components/Home/ColorMarker/ColorMarker";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./SACScreen.css";
+import { listsacLinks } from "../../../actions/sac";
+import LinkCard from "../utilities/LinkCard";
 
-// const SACScreen = () => {
-//   const sacs = useSelector((state) => state.sacs);
-//   const dispatch = useDispatch();
+////// This component is using "LinkCard" and its child component "SubLinks"
+////// from UtilitiesScreen and any debugging in these need to be done there
 
-//   useEffect(() => {
-//     dispatch(listSACs());
-//   }, [dispatch]);
+const SACScreen = () => {
+  const sacs = useSelector((state) => state.sacs);
+  const dispatch = useDispatch();
 
-//   return (
-//     <div className="sac_cont container w-full mx-auto p-6 sm:py-12 sm:px-36">
-//       <div className="flex justify-between w-full mb-4 sm:mb-6">
-//         <p className="text-2xl sm:text-3xl font-bold">SAC</p>
-//         <ColorMarker />
-//       </div>
-//       {sacs.map((sac, idx) => {
-//         const { _id, name, path, format } = sac;
-//         return (
-//           <div
-//             key={_id}
-//             className="flex w-full justify-start mb-2 p-3 rounded-md"
-//             style={{ border: "1px solid #D7DDE1" }}
-//           >
-//             <span
-//               className="w-1/12 flex justify-center items-center"
-//               style={{ borderRight: "1px solid #D7DDE1" }}
-//             >
-//               {idx + 1}
-//             </span>
-//             <span className="w-11/12 pl-4 flex justify-start items-center">
-//               {path.indexOf("https://") === -1 ? (
-//                 <a
-//                   className={`${
-//                     format === "Link" ? "text-blue-500" : "text-red-500"
-//                   } hover:text-blue-900`}
-//                   href={`${BASEAPI}/sac/${_id}`}
-//                   rel="noreferrer"
-//                   target="_blank"
-//                   style={{ fontFamily: "Red Hat Display", fontWeight: "500" }}
-//                 >
-//                   {name}
-//                 </a>
-//               ) : (
-//                 <a
-//                   className={`${
-//                     format === "Link" ? "text-blue-500" : "text-red-500"
-//                   } hover:text-blue-900`}
-//                   href={path}
-//                   rel="noreferrer"
-//                   target="_blank"
-//                   style={{ fontFamily: "Red Hat Display", fontWeight: "500" }}
-//                 >
-//                   {name}
-//                 </a>
-//               )}
-//             </span>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    dispatch(listsacLinks());
+  }, [dispatch]);
 
-// export default SACScreen;
+  useEffect(() => {
+    console.log("SACs data", sacs);
+  }, [sacs]);
+
+  return (
+    <div className="container w-full mx-auto p-6 sm:py-12 sm:px-36">
+      <p className="text-2xl sm:text-3xl font-bold mb-6">SAC</p>
+      {sacs &&
+        sacs.map((sac, idx) => {
+          return <LinkCard key={sac._id} details={sac} />;
+        })}
+    </div>
+  );
+};
+
+export default SACScreen;
